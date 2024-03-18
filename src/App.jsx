b537e05038f1843d454b03585d871deb6f97597d
muhammadnaseer44 <naseerbaloch110@outlook.com>
2024-03-18 20:30:13 +0500

    edit context

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Demo from './Components/Basics/Demo'
+import Todo from './Components/Context/Todo/Todo'
 
 function App() {
   return (
     <div className='text-center'>
       <Demo />
+      <Todo />
     </div>
   )
 }

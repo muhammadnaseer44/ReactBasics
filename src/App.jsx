867711a625590fe342ed70704110d1d0e62b74cd
muhammadnaseer44 <naseerbaloch110@outlook.com>
2024-05-05 11:21:11 +0500

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import Demo from './Components/Basics/Demo'
-import Form1 from './Components/Forms/Form1'
-import Form2 from './Components/Forms/Form2'
-import Form3 from './Components/Forms/Form3'
+import CardItem from './Components/Cards/CardItems'
 
 function App() {
   return (
     <div className='text-center'>
       <Demo />
-      <Form1 />
-      <Form2 />
-      {/* <Form3 /> */}
+      <CardItem />
     </div>
   )
 }
